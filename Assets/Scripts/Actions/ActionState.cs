98f511c2236431d57993b7e56bb83192184f90f8
JP314 <jan.grau95@gmail.com>
2016-06-04 21:15:38 +0200

    Revert "Revert "Behaviour + New Vector Extension""
    This reverts commit 96203ce2d681b1bf4dfb3bc4a76ce1ef57c642dd.

---
+++ Assets/Scripts/Actions/ActionState.cs
@@ -0,0 +1,3 @@
+﻿public enum ActionState {
+	NotStarted, Runnning, Finished
+}
---
+++ Assets/Scripts/Actions/ParallelBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ParallelBehaviour : ActionBehaviour {
+
+	public List<ActionBehaviour> behaviours;
+
+	public override void OnStartAction ()
+	{
+		base.OnStartAction ();
+		this.duration = behaviours.Max (action => action.duration);
+	}
+
+}
